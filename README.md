45866ce38379d32d73d9cfe168f03a30fe22a747
lilu <lilugirl2005@126.com>
2020-05-02 13:30:19 +0800

    static

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # koa-first-demo
-koa koa-router koa-body
+koa koa-router koa-body koa-jsonn @koa/cors koa-combine-routers koa-helmet koa-static 的基本使用
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const koa = require('koa');
+const path = require('path');
 const app = new koa();
+const helmet = require('koa-helmet');
+const statics = require('koa-static');
 
 const router = require('./routes/routes')
 
+app.use(helmet())
+app.use(statics(path.join(__dirname, '../public')))
 app.use(router())
-
+console.log(path.join(__dirname, '../public'))
 app.listen(3000)
