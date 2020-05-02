b3d9fe91f3b77e5a10192ce06df6c4427fa4c821
lilu <lilugirl2005@126.com>
2020-05-02 14:22:04 +0800

    使用es6语法开发和调试，使用webpack打包

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-const koa = require('koa');
-const path = require('path');
+// const koa = require('koa');
+// const path = require('path');
+import koa from 'koa';
+import path from 'path';
 const app = new koa();
-const helmet = require('koa-helmet');
-const statics = require('koa-static');
+// const helmet = require('koa-helmet');
+//const statics = require('koa-static');
+import helmet from 'koa-helmet';
+import statics from 'koa-static';
 
-const router = require('./routes/routes')
+// const router = require('./routes/routes')
+import router from './routes/routes'
 
 app.use(helmet())
 app.use(statics(path.join(__dirname, '../public')))
